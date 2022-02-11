83f790a05d9cec6334e9c7d63dcf75b1621065ca
oawood93 <oa.wood@outlook.com>
2022-02-11 15:00:51 +0000

    refactor

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,36 +1,55 @@
 
 public class TennisGame3 implements TennisGame {
     
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int player1Score;
+    private int player2Score;
+    private String player1Name;
+    private String player2Name;
+    private String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+
+        if (isGameInPlay()) {
+            var player1Points = points[player1Score];
+            var player2Points = points[player2Score];
+
+            return isTied()
+                    ? player1Points + "-All"
+                    : player2Points + "-" + player1Points;
+
         }
+
+
+        if (isTied())
+            return "Deuce";
+
+        var inLeadPlayer = player2Score > player1Score ? player1Name : player2Name;
+        return (checkForWin() ? "Win for " : "Advantage ") + inLeadPlayer;
+
     }
     
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.player2Score += 1;
         else
-            this.p2 += 1;
+            this.player1Score += 1;
         
     }
 
+    public boolean isGameInPlay(){
+        return player2Score < 4 && player1Score < 4 && !(player2Score + player1Score == 6);
+    }
+
+    public boolean isTied(){
+        return player2Score == player1Score;
+    }
+
+    public boolean checkForWin() {
+        return Math.abs(player1Score - player2Score) > 1;
+    }
 }
