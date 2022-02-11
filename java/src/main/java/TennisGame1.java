fea3dc369c5d137ac855cc6c38102c758eb98a26
iagardi <garbigz@gmail.com>
2022-02-11 13:57:31 +0000

    refactor

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,45 +1,58 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    private String playerOneName;
+    private String playerTwoName;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals(player1Name))
-            m_score1 += 1;
+        if (playerName.equals(playerOneName))
+            playerOneScore += 1;
 
-        if (playerName.equals(player2Name))
-            m_score2 += 1;
+        if (playerName.equals(playerTwoName))
+            playerTwoScore += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1 == m_score2)
+
+        if (playerOneScore == playerTwoScore)
         {
-            score = getTiedScore(m_score1);
+            return getTiedScore(playerOneScore);
         }
-        else if (m_score1>=4 || m_score2>=4)
+
+        if (playerOneScore >=4 || playerTwoScore >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            int scoreDifference = getScoreDifference(playerOneScore, playerTwoScore);
+            if (playerOneScore > playerTwoScore) {
+                if (scoreDifference == 1) {
+                    score = "Advantage player1";
+                }
+                else {
+                    score = "Win for player1";
+                }
+            }
+            if (playerOneScore < playerTwoScore) {
+                if (scoreDifference == 1) {
+                    score = "Advantage player2";
+                } else {
+                    score = "Win for player2";
+                }
+            }
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = playerOneScore;
+                else { score+="-"; tempScore = playerTwoScore;}
                 switch(tempScore)
                 {
                     case 0:
@@ -72,6 +85,9 @@ public class TennisGame1 implements TennisGame {
             default:
                 return "Deuce";
         }
+    }
 
+    private int getScoreDifference(int playerOneScore, int playerTwoScore){
+        return Math.abs(playerOneScore - playerTwoScore);
     }
 }
